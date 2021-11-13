d4244c9b3c7fe43655e756529cb81bc35c55c74d
Semperj06 <94219596+Semperj06@users.noreply.github.com>
2021-11-13 16:12:26 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+#Exercise 1.print("Hello world")
+
+'''Exercise 2.
+print(end='\n%    '"My name David")
+print(end='\n-      '"I am from Ukraine")
+print(end='\n=      '"life in Ukraine is hard")'''
+'''Exercise 3.
+length=int(input("enter length rectangle"))
+width=int(input("enter width rectangle"))
+area=length*width
+print("The area of the rectangle is",area)'''
+
+"""Exercise 4.
+FirstNo = int(input("Enter first number"))
+SecondNo = int(input("Enter the second number"))
+Sum = (FirstNo + SecondNo)
+Product = FirstNo * SecondNo
+Difference = FirstNo - SecondNo
+Quotient = FirstNo / SecondNo
+Remainder = FirstNo % SecondNo
+
+print(Sum)
+print(Product)
+print(Quotient)
+print(Remainder)"""
+
+
+Radius = float(input("Enter the radius of the circle"))
+P = float(3.14159)
+Diameter = float(Radius*2)
+Circumference = float(2*P*Diameter)
+Area = float(P*Radius**2)
+print("Diameter are", '\n', Diameter)
+print("Circumference are", '\n', Circumference)
+print("Area are", '\n', Area)
