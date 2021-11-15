61dbf3eb20ef303d0f21aac3a6b25eb4368a46b2
Semperj06 <94219596+Semperj06@users.noreply.github.com>
2021-11-15 20:29:32 +0200

    Add files via upload

---
+++ home work.py
@@ -0,0 +1,28 @@
+# Exercise 1.
+# x = str(123)
+# x = int(x[0]+x[1]+x[2])
+# Exercise 2.
+# x = int(123)
+# x = float((x * 100) / 100)
+'''#Exercise 3.
+#x = float(12.345)
+#x = int(x*1000)
+#x = round(x)
+#Exercise 4.
+#numb = str(input("Enter number card"))
+#print("last four number", numb[12], numb[13], numb[14], numb[15])
+#Exercise 5.
+x = str(input("Enter number"))
+a = int(x[0])
+b = int(x[1])
+c = int(x[2])
+print("sum", a+b+c)
+#Exercise 6.
+a = int(input("enter variable a ="))
+b = int(input("enter variable b ="))
+c = int(input("enter variable c ="))
+p = (a + b + c) / 2
+s = p * (p - a) * (p - b) * (p - c)
+import math
+s = math.sqrt(s)
+print("area of a triangle =", s)'''
