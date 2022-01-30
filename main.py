76fe63350edda8f94d9102f162d10e1458bcf65e
Semperj06 <94219596+Semperj06@users.noreply.github.com>
2022-01-30 18:45:26 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,47 @@
-
 from flask import Flask, request
 import telebot
 import os
 
 
 app = Flask(__name__)
-TOKEN = os.environ.get("TOKEN")
+TOKEN = ""
 bot = telebot.TeleBot(TOKEN)
 
 
-@bot.message_handler(commands=["start"])
+@bot.message_handler(commands=['start'])
 def message_start(message):
-    bot.send_message(message.chat.id, "Hello, user!")
+    bot.send_message(message.chat.id, 'Hello, user!')
+
+@bot.message_handler(commands=['courses'])
+def message_courses(message):
+    keyboard = telebot.types.InlineKeyboardMarkup(row_width=1)
+
+    with open('courses.txt') as file:
+        courses = [item.split(',') for item in file]
+
+        for title, link in courses:
+            url_button = telebot.types.InlineKeyboardButton(text=title.strip(), url=link.strip())
+            keyboard.add(url_button)
+
+        bot.send_message(message.chat.id, 'List of courses', reply_markup=keyboard)
+
+@bot.message_handler(func=lambda x: x.text.lower().startswith('python'))
+def message_text(message):
+    bot.send_message(message.chat.id, 'Python')
 
 
-@app.route("/" + TOKEN, methods=["POST"])
+@app.route('/' + TOKEN, methods=['POST'])
 def get_message():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-    return "Python Telegram Bot 31-01-2022", 200
+    return "Python Telegram Bot 30-01-2022", 200
 
 
-@app.route("/")
+@app.route('/')
 def main():
     bot.remove_webhook()
-    bot.set_webhook(url="https://semperj06.herokuapp.com/" + TOKEN)
-    return "Python Telegram Bot", 200
+    bot.set_webhook(url='https://prog-kyiv-jan.herokuapp.com/' + TOKEN)
+    return 'Python Telegram Bot', 200
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
